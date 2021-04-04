e5ff23a7b36f492901f19b1e91220217c20f05c0
Konstantin Shchygolev <kostas@welldone-software.com>
2021-04-04 21:44:10 +0300

    first commit

---
+++ src/components/AddExpenseForm.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { AppContext } from "../context/AppContext";
+import { v4 as uuidv4 } from "uuid";
+
+const AddExpenseForm = () => {
+  const { dispatch } = React.useContext(AppContext);
+  const [name, setName] = React.useState("");
+  const [cost, setCost] = React.useState("");
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+
+    const expense = {
+      id: uuidv4(),
+      name: name,
+      cost: parseInt(cost),
+    };
+
+    dispatch({
+      type: "ADD_EXPENSE",
+      payload: expense,
+    });
+  };
+
+  return (
+    <form onSubmit={onSubmit}>
+      <div className="row">
+        <div className="col-sm">
+          <label for="name">Name</label>
+          <input
+            required="required"
+            type="text"
+            className="form-control"
+            id="name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          ></input>
+        </div>
+        <div className="col-sm">
+          <label for="cost">Cost</label>
+          <input
+            required="required"
+            type="text"
+            className="form-control"
+            id="cost"
+            value={cost}
+            onChange={(event) => setCost(event.target.value)}
+          ></input>
+        </div>
+        <div className="col-sm">
+          <button type="submit" className="btn btn-primary">
+            Save
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default AddExpenseForm;
---
+++ src/components/Budget.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { AppContext } from "../context/AppContext";
+
+const Budget = () => {
+  const { budget } = React.useContext(AppContext);
+
+  return (
+    <div className="alert alert-secondary">
+      <span>Budget: ${budget}</span>
+    </div>
+  );
+};
+
+export default Budget;
